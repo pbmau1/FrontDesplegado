2f8b3d7fc128a1a7c4e8b0dfea0c819bd2260fa3
mau <mauanto007@gmail.com>
2025-12-01 22:16:50 -0500

    no

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  base: 'https://frontmoney.netlify.app',
+  base: 'http://localhost:8080',
+  basefront: 'https://frontmoney.netlify.app',
   newsUrl: 'https://newsapi.org/v2',
   newsApiKey: '2abe58a822be4684a0f7883607d63e4f'
 };
