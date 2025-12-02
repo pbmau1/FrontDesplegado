82e505f5079fe34c6ce711ca586033e640398536
mau <mauanto007@gmail.com>
2025-12-02 01:19:41 -0500

    log

--- src/app/services/login-service.ts
+++ src/app/services/login-service.ts
@@ -7,7 +7,7 @@ import { JwtRequestDTO } from '../models/JwtRequestDTO';
   providedIn: 'root',
 })
 export class LoginService {
-  private apiUrl = 'http://backend-5v10.onrender.com/swagger-ui/index.html/login';
+  private apiUrl = 'http://backend-5v10.onrender.com/login';
 
   constructor(private http: HttpClient) {}
 
