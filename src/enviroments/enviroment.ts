da746c34ca92364db8e35f4d881dda2b57319b5b
mau <mauanto007@gmail.com>
2025-12-02 01:27:50 -0500

    environments

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  base: 'https://backend-5v10.onrender.com/swagger-ui/index.html',
+  base: 'https://backend-5v10.onrender.com',
   newsUrl: 'https://newsapi.org/v2',
   newsApiKey: '2abe58a822be4684a0f7883607d63e4f'
 };
